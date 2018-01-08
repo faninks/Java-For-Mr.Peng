bb9201fd5dfa1415acc14c1147021d5979522dd3
white3 <1070072593@qq.com>
2018-01-08 22:32:11 +0800

    Too tired.

--- src/com/model/Department.java
+++ src/com/model/Department.java
@@ -1,36 +1,95 @@
 package com.model;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
 
-public class Department{
+public class Department implements Comparable<Department>{
 	// 部门  员工  职位
-	private ArrayList<Staff> staffs;
+	private TreeSet<Staff> staffs;
 	private String name;
-
-	public Department() {
-		super();
-	}
-
-	public Department(String name) {
-		super();
-		this.name = name;
-	}
+	private String dNum; // 002x001 -> 002 = dNum
 	
-	public ArrayList<Staff> getStaffs() {
-		return staffs;
-	}
-
-	public void setStaffs(ArrayList<Staff> staffs) {
-		this.staffs = staffs;
+	@Override
+	public int compareTo(Department o) {
+		if(Integer.valueOf(o.getdNum())>Integer.valueOf(dNum))
+			return 1;
+		else if(Integer.valueOf(o.getdNum()).equals(Integer.valueOf(dNum)))
+			return 0;
+		return -1;
 	}
 
 	@Override
 	public String toString() {
-		return "DepartmentImpl [staffs=" + staffs + "]";
+		String str = "部门名称: " + name;
+		if(staffs!=null) {
+			str += " 人员如下:\n";
+			for (Iterator<Staff> iterator = staffs.iterator(); iterator.hasNext();) {
+				Staff staff = iterator.next();
+				str += staff.toString() + "\n";
+			}
+		}
+		return str;
+	}
+	
+	public String toStringEasy() {
+		String str = "部门名称: " + name;
+		if(staffs!=null) {
+			str += " 人员如下:\n";
+			for (Iterator<Staff> iterator = staffs.iterator(); iterator.hasNext();) {
+				Staff staff = iterator.next();
+				str += staff.getJob().getJobName() + staff.getName() + "\n";
+			}
+		}
+		return str;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dNum == null) ? 0 : dNum.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (dNum == null) {
+			if (other.dNum != null)
+				return false;
+		} else if (!dNum.equals(other.dNum))
+			return false;
+		return true;
+	}
+
+	public TreeSet<Staff> getStaffs() {
+		return staffs;
+	}
+
+	public void setStaffs(TreeSet<Staff> staffs) {
+		this.staffs = staffs;
 	}
 
 	public String getName() {
-		// TODO Auto-generated method stub
 		return name;
-	} 
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getdNum() {
+		return dNum;
+	}
+
+	public void setdNum(String dNum) {
+		this.dNum = dNum;
+	}
+	
 }
--- src/com/view/view.java
+++ src/com/view/view.java
@@ -15,20 +15,12 @@ public class view {
 		// TODO Auto-generated method stub
 		views();
 	}
-	//1.员工查询
-	//A、个人信息、B、工资情况、C、请假情况、D、加班情况
-	//2.部门查询
-	//A、人员一览、B、加班一览、C、请假一览
-	//3.工资全览
-	//A、所有部门工资一览、B、所有员工工资一览、C、工资提升率分布显示
+
 	public static void views() {
 		Scanner cin = new Scanner(System.in);
-		boolean flag, flag2;
 		String ch;
 		System.out.println("欢迎进入工资管理系统");
-		while(true) {
-			flag = false;
-			flag2 = false;
+		while (true) {
 			ch = "";
 			System.out.println("1.员工查询");
 			System.out.println("2.部门查询");
@@ -36,70 +28,107 @@ public class view {
 			System.out.println("quit.退出系统");
 			System.out.println("请输入你的选择");
 			ch = cin.nextLine();
-			if("1".equals(ch)) {
-				while(true) {
-					System.out.println("-----------------------------------------");
-					System.out.println("请输入(部分)用户信息格式如下:");
-					System.out.println("姓名|性别|职工号|年龄|工资级别 +[空格]+[信息] , 或输入信息");
-					System.out.println("后退请输入q");
-					ch = cin.nextLine();
-					if("q".equals(ch))
-						break;
-					else {
-						TreeSet<Staff> sts = StaffServlet.quaryFind(ch);
-						if(sts.isEmpty())
-							System.out.println("搜索结果为空哦~");
-						else {
-							flag = false;
-							flag2 = false;
-							System.out.println("是否显示详细的请假记录？(1/0|y/n)");
-							ch = cin.nextLine();
-							if("1".equals(ch)||"y".equals(ch))
-								flag = true;
-							System.out.println("是否显示详细的加班记录？(1/0|y/n)");
-							ch = cin.nextLine();
-							if("1".equals(ch)||"y".equals(ch))
-								flag2 = true;
-							System.out.println("-----------------------------------------");
-							for (Iterator<Staff> iterator = sts.iterator(); iterator.hasNext();) {
-								Staff staff = iterator.next();
-								ch = staff.toString() + "\n";
-								if(flag) {
-									staff.setLvEvents(LeaveEventServlet.quaryFind("职工号", staff.getStaffNum()));
-									ch += staff.toStringLeaveEvent().trim() + "\n";
-								}
-								if(flag2) {
-									staff.setHwEvents(HardworkEventServlet.quaryFind("职工号", staff.getStaffNum()));
-									ch += staff.toStringhardworkEvent().trim() + "\n";
-								}
-								System.out.print(ch);
-							}
-						}
-						System.out.println("是否继续(1/0|y/n)");
-						ch = cin.nextLine();
-						if("1".equals(ch)||"y".equals(ch))
-							continue;
-						else {
-							break;
-						}
-					}
-				}
-			}else if("2".equals(ch)) {
-				System.out.println("A、人员一览、");
-				System.out.println("B、加班一览、");
-				System.out.println("C、请假一览、");
-				System.out.println("D、后退");
-			}else if("3".equals(ch)) {
-				System.out.println("A、所有部门工资一览、");
-				System.out.println("B、所有员工工资一览、");
-				System.out.println("C、工资提升率分布显示、");
-				System.out.println("D、后退");
-			}else if("quit".equals(ch)) {
+			if ("1".equals(ch)) {
+				view0(cin);
+			} else if ("2".equals(ch)) {
+				view1(cin);
+			} else if ("3".equals(ch)) {
+				view2(cin);
+			} else if ("quit".equals(ch)) {
 				break;
-			}else {
+			} else {
 				System.out.println("输入异常,请重新输入");
 			}
 		}
 		cin.close();
+	}
+
+	// 1.员工查询
+	// A、个人信息、B、工资情况、C、请假情况、D、加班情况
+	public static void view0(Scanner cin) {
+		boolean flag, flag2;
+		String ch;
+		while (true) {
+			System.out.println("-----------------------------------------");
+			System.out.println("请输入(可部分)用户信息格式如下:");
+			System.out.println("姓名|性别|职工号|年龄|工资级别 +[空格]+[信息] , 或输入信息");
+			System.out.println("后退请输入q");
+			ch = cin.nextLine();
+			if ("q".equals(ch.trim())||"Q".equals(ch.trim()))
+				break;
+			else {
+				TreeSet<Staff> sts = StaffServlet.quaryFind(ch);
+				if (sts.isEmpty())
+					System.out.println("搜索结果为空哦~");
+				else {
+					flag = false;
+					flag2 = false;
+					System.out.println("是否显示详细的请假记录？(1/0|y/n)");
+					ch = cin.nextLine();
+					if ("1".equals(ch.trim()) || "y".equals(ch.trim()))
+						flag = true;
+					System.out.println("是否显示详细的加班记录？(1/0|y/n)");
+					ch = cin.nextLine();
+					if ("1".equals(ch.trim()) || "y".equals(ch.trim()))
+						flag2 = true;
+					System.out.println("-----------------------------------------");
+					for (Iterator<Staff> iterator = sts.iterator(); iterator.hasNext();) {
+						Staff staff = iterator.next();
+						ch = staff.toString() + "\n";
+						if (flag) {
+							staff.setLvEvents(LeaveEventServlet.quaryFind("职工号", staff.getStaffNum()));
+							ch += staff.toStringLeaveEvent().trim() + "\n";
+						}
+						if (flag2) {
+							staff.setHwEvents(HardworkEventServlet.quaryFind("职工号", staff.getStaffNum()));
+							ch += staff.toStringhardworkEvent().trim() + "\n";
+						}
+						System.out.print(ch);
+					}
+				}
+				System.out.println("是否继续(1/0|y/n)");
+				ch = cin.nextLine();
+				if ("1".equals(ch) || "y".equals(ch))
+					continue;
+				else
+					break;
+			}
+		}
+	}
+
+	// 2.部门查询
+	// A、人员一览、B、加班一览、C、请假一览、D、部门详情、
+	public static void view1(Scanner cin) {
+		String ch = "";
+		while (true) {
+			System.out.println("-----------------------------------------");
+			System.out.println("请输入选项: ");
+			System.out.println("A、人员一览、");
+			System.out.println("B、加班一览、");
+			System.out.println("C、请假一览、");
+			System.out.println("D、部门详情、");
+			System.out.println("q、后退");
+			ch = cin.nextLine();
+			switch(ch.trim()) {
+				case "A":case "a":
+					
+				case "B":case "b":
+				case "C":case "c":
+				case "D":case "d":
+			}
+			if("q".equals(ch.trim())||"Q".equals(ch.trim()))
+				break;
+		}
+	}
+
+	// 3.工资全览
+	// A、所有部门工资一览、B、所有员工工资一览、C、工资提升率分布显示
+	public static void view2(Scanner cin) {
+		while (true) {
+			System.out.println("A、所有部门工资一览、");
+			System.out.println("B、所有员工工资一览、");
+			System.out.println("C、工资提升率分布显示、");
+			System.out.println("D、后退");
+		}
 	}
 }
