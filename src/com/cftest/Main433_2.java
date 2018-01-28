684dad6c9ab46517dd925ce8943f20837e560302
white3 <1070072593@qq.com>
2018-01-28 17:46:40 +0800

    111

--- src/com/cftest/Main433_2.java
+++ src/com/cftest/Main433_2.java
@@ -1,10 +1,28 @@
 package com.cftest;
 
+import java.math.*;
 import java.util.*;
 public class Main433_2 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		int n = in.nextInt(), flag = 0, win = 0;
+		BigInteger k = in.nextBigInteger();
+		LinkedList<Integer> a = new LinkedList<>();
+		win = in.nextInt();
+		for(int i=1; i<n; ++i)
+			a.add(in.nextInt());
+		while(flag==k.intValue()) {
+			if(win == a.getFirst()) { 
+				flag++;
+				a.add(a.remove());
+			}else {
+				flag = 0;
+				a.add(win);
+				win = a.remove();
+			}
+		}
+		System.out.println(win);
 		in.close();
 	}
 
