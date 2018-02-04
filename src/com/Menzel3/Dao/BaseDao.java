33ee31840158866ee9ae69e001d37c398069cda8
white3 <1070072593@qq.com>
2018-02-04 20:38:12 +0800

    0

---
+++ src/com/Menzel3/Dao/BaseDao.java
@@ -0,0 +1,9 @@
+package com.Menzel3.Dao;
+
+public class BaseDao {
+
+	
+	public static void update(String sql, Object ... objs) {
+		
+	}
+}
---
+++ src/com/Menzel3/Dao/Dao.java
@@ -0,0 +1,5 @@
+package com.Menzel3.Dao;
+
+interface Dao {
+
+}
